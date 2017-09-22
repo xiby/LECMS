877bdae3e5b9d56bf9ff8b868bbf78df779d3939
xiby <15555438336@163.com>
2017-09-22 08:49:49 +0800

    增加admin模块

--- myApp/urls.py
+++ myApp/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from . import views
+from django.contrib import admin
 
-urlpatterns={
-    url(r'^$',views.index,name='index'),
+urlpatterns=[
+    url(r'^admin/',admin.site.urls),
     url(r'^login/',views.login,name="login"),
-}
+]
